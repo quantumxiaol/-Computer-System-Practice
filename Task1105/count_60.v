6d4365243961d5b47026c4293b359d9dc8e2dfda
quantumxiaol <liuxiaolong.0425@foxmail.com>
2022-11-03 16:00:16 +0800

    v60

--- Task1105/count_60.v
+++ Task1105/count_60.v
@@ -17,7 +17,7 @@ module count_60(
         .count (cout10 ),
         .co    (co1    )
     );    
-    and u_and_10(co10,co,co1)
+    and u_and_10(co10,en,co1);
     count_6 u_count_6(
     	.rst   (rst   ),
         .clk   (clk   ),
@@ -25,7 +25,7 @@ module count_60(
         .count (cout6 ),
         .co    (co6    )
     );
-    and u_and_6(co,co10,co6)
+    and u_and_6(co,co10,co6);
 
 assign count ={cout6,cout10} ; //模60计数器的输出
 
@@ -36,26 +36,30 @@ endmodule
     input wire clk,
     input wire en,
     output reg [3:0] count,
-    output reg co
+    output  co
 );
     always @ (posedge clk or negedge rst) begin
         if (rst) begin
             count <= 4'b0000;
-            co <= 1'b0;
+            //co <= 1'b0;
         end
         else if (en) begin
             if (count == 4'd9) begin
-                //count <= 4'b0;
-                co <= 1'b1;
+                //co <= 1'b1;                
+                count <= 4'b0;
+
             end
             else begin
                 count <= count + 4'b1;
-                co <= 1'b0;
+                //co <= 1'b0;
             end
         end
+        else begin
+            count <= count;
+        end        
         
-        //assign co <= count[0]&count[3];  //仅当计数达到9(4'b1001)时，进位为1
     end
+    assign co <= count[0]&count[3];  //仅当计数达到9(4'b1001)时，进位为1
     endmodule
 
     module count_6(
@@ -63,26 +67,30 @@ endmodule
     input wire clk,
     input wire en,
     output reg [3:0] count,
-    output reg co
+    output  co
 );
     always @ (posedge clk) begin
         if (rst) begin
             count <= 4'b0000;
-            co <= 1'b0;
+            //co <= 1'b0;
         end
         else if (en) begin
             if (count == 4'd5) begin
+                //co <= 1'b1;                
                 count <= 4'b0;
-                co <= 1'b1;
+
             end
             else begin
-                count <= count + 4'd9;
-                co <= 1'b0;
+                count <= count + 4'b1;
+                //co <= 1'b0;
             end
         end
-
-        //assign co <= count[0]&count[2];  //仅当计数达到5(4'b0101)时，进位为1
+        else begin
+            count <= count;
+        end
+        
     end
+    assign co <= count[0]&count[2];  //仅当计数达到5(4'b0101)时，进位为1
     endmodule
 
 
